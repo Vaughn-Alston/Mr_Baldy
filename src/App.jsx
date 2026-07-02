c603760ce94ed9f757f82a168a32253915d17baa
Vaughn-Alston <vaughn2code@gmail.com>
2026-07-02 10:25:19 -0700

    I have my api to log on the console.

--- src/App.jsx
+++ src/App.jsx
@@ -4,70 +4,52 @@ import DataCard from "./dataCard";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import mainMusicAsset from './assets/audio/mainMusic.wav';
+import { Typography } from '@mui/material';
+import { useState, useEffect } from "react";
+const myAPIkey = import.meta.env.DINO_API_KEY;
 
 function App() {
+  //   const playBuzz = () => {
 
+  //   const audio = new Audio(mainMusicAsset);
 
-  
+  //   audio.loop = true; // Set the volume to 10%
+  //   audio.play();
+  // };
 
+  //  This will be the API key that I will be using to get the data from the API
 
-  const playBuzz = () => {
+  const selectedCategory = 'love'; // Replace with the desired category
 
-  const audio = new Audio(mainMusicAsset);
+  // API Ninjas doesnt not allow you to pass the api key in the URL so I will have to pass it in the header of the fetch request
+  const baseURL = `https://api.api-ninjas.com/v2/categories/?${selectedCategory}`;
 
-  audio.loop = true; // Set the volume to 10%
-  audio.play();
-};
-
-
-
-
-// This line of code will fetch the API from the link 
-// Then the next line of code will then take that response which is a raw json data file and make it readable
-//Then the next line will take that read able data and setfunFact, then we will grab the fact from the data object
-// The bottom line will catch any errors
-  const searchDogFunFact = () => {
-    fetch(`https://dogapi.dog/api/v1/facts?number=2`)
-      .then((response) => (response.json()))
-      // add data to state
-      .then((data) => setfunFact(data.facts))
-      
-      // this error function is pretty standard so I will leave this here
-      .catch((error) => console.error(error));
-  };
-  // add a log to check your state
+  const [data, setData] = useState([]);
 
 
 
+  fetch("https://api.api-ninjas.com/v2/quotes",
+    {
+      method: 'GET',
+      headers: {
+        'X-Api-Key':'ilfaf1OxzW6pvhSH1cQ3KbYFix0nECtgRTpoR3o0'
+      }
+    })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("got data", result);
+      // setData(result);
+    })
+    .catch(error => console.log("error", error));
 
-
-
-  return (
-
-    <>
-     
-
-
-{/* Above the button I want to put a search bar that will hold the user type response */}
-
-
-<Box sx={{ width: 500, maxWidth: '100%' }}>
+return (
+  <>
+    {/* Above the button I want to put a search bar that will hold the user type response */}
+    <Box sx={{ width: 500, maxWidth: '100%' }}>
       <TextField fullWidth label="fullWidth" id="fullWidth" />
-</Box>
-
-
-
-
-
-   {/* Here I Will create the UI That will act as a tablet for the user to input there response */}
-
-
-
-
-
-
-
-     <ButtonGroup
+    </Box>
+    {/* Here I Will create the UI That will act as a tablet for the user to input there response */}
+    <ButtonGroup
       disableElevation
       variant="contained"
       aria-label="Disabled button group"
@@ -75,91 +57,54 @@ function App() {
       <Button>1</Button>
       <Button>2</Button>
       <Button>3</Button>
-
-
-
-
-
-
-
     </ButtonGroup>
+    <div>
+      <img src="src/assets/images/App.jsx.jpeg" alt="Description" />
 
-
-
-<div>   
-
-
-
-<img src="src/assets/images/App.jsx.jpeg" alt="Description" />
-
-</div>
-
-
-
+      <Typography variant="h6" component="div" gutterBottom>
+        {data}
+      </Typography>
+    </div>
 
       <ButtonGroup
-      disableElevation
-      variant="contained"
-      aria-label="Disabled button group"
-    >
-      <Button>4</Button>
-      <Button>5</Button>
-      <Button>6</Button>
-    </ButtonGroup>
-
-      <ButtonGroup
-      disableElevation
-      variant="contained"
-      aria-label="Disabled button group"
-    >
-      <Button>7</Button>
-      <Button>8</Button>
-      <Button>9</Button>
-    </ButtonGroup>
-    
-
-       <ButtonGroup
-      disableElevation
-      variant="contained"
-      aria-label="Disabled button group"
-    >
-      <Button>0</Button>
-      <Button>Clear</Button>
-
+        disableElevation
+        variant="contained"
+        aria-label="Disabled button group"
+      >
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
       </ButtonGroup>
 
-       <ButtonGroup
-      disableElevation
-      variant="contained"
-      aria-label="Disabled button group"
-    >  
-      <Button>Backspace</Button>
+      <ButtonGroup
+        disableElevation
+        variant="contained"
+        aria-label="Disabled button group"
+      >
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
       </ButtonGroup>
 
 
+      <ButtonGroup
+        disableElevation
+        variant="contained"
+        aria-label="Disabled button group"
+      >
+        <Button>0</Button>
+        <Button>Clear</Button>
 
+      </ButtonGroup>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      <ButtonGroup
+        disableElevation
+        variant="contained"
+        aria-label="Disabled button group"
+      >
+        <Button>Backspace</Button>
+      </ButtonGroup>
     </>
-
-
-
   );
 }
-
 export default App;
